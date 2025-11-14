58471cd2ba06fa18175272ef1722e67d278b8350
Mohamed Fateen F <fateen1028@gmail.com>
2025-11-14 22:06:48 +0530

    2

--- Frontend/src/components/chatbot.jsx
+++ Frontend/src/components/chatbot.jsx
@@ -29,7 +29,7 @@ const ChatBot = () => {
     ]);
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/chat", {
+      const res = await fetch("https://fateen-portfolio-ai.vercel.app/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: input }),
